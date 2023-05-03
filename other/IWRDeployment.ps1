340125c56085b5fa781f0d9f9d259a3bb25ebb78
OlsenSM91 <130707762+OlsenSM91@users.noreply.github.com>
2023-05-03 09:55:08 -0700

    Update IWRDeployment.ps1
    Updated script for greater success during deployment on workstation

--- other/IWRDeployment.ps1
+++ other/IWRDeployment.ps1
@@ -1,45 +1,34 @@
+# Use TLS 1.2
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
 # Get the current script path
 $scriptPath = Split-Path -Parent -Path $MyInvocation.MyCommand.Definition
 
-# Define the registry path
-$registryPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Installer\Folders"
-
-# Define the search string
-$searchString = "C:\Windows\LTScv"
-
 # Define the uninstaller and installer URLs
 $uninstallerUrl = "https://github.com/OlsenSM91/ConnectWise-Automate-Cleaner/releases/download/Rev1/Agent_Uninstall.exe"
-$installerUrl = "https://cns4u.hostedrmm.com/LabTech/Deployment.aspx?InstallerToken=779eb46901ab41039e9848e61f3b0a0f"
+$installerUrl = "INSERT_CUSTOM_AGENT_MSI_DOWNLOAD_LINK_HERE"
 
 # Define the local file names
 $uninstaller = Join-Path -Path $scriptPath -ChildPath "Agent_Uninstall.exe"
 $installer = Join-Path -Path $scriptPath -ChildPath "Agent_Install.msi"
 
-# Download the uninstaller and installer
-Invoke-WebRequest -Uri $uninstallerUrl -OutFile $uninstaller
-Invoke-WebRequest -Uri $installerUrl -OutFile $installer
+try {
+    # Download the uninstaller and installer
+    Invoke-WebRequest -Uri $uninstallerUrl -OutFile $uninstaller
+    Invoke-WebRequest -Uri $installerUrl -OutFile $installer
 
-# Check if the user has administrator privileges
-if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-    Write-Output "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
-    return
-}
-
-# Check if the registry key exists and contains the search string
-$registryKeys = Get-ItemProperty -Path $registryPath
-
-$found = $false
-foreach ($key in $registryKeys.PSObject.Properties) {
-    if ($key.Value -eq $searchString) {
-        $found = $true
-        break
+    # Check if the user has administrator privileges
+    if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+        throw "You do not have Administrator rights to run this script!`nPlease re-run this script as an Administrator!"
     }
-}
 
-if ($found) {
-    # If found, run the uninstaller
+    # Run the uninstaller
     Start-Process -FilePath $uninstaller -Verb RunAs -Wait
-}
 
-# Then, run the installer (MSI)
-Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$installer`" /qn" -Verb RunAs -Wait
+    # Then, run the installer (MSI)
+    Start-Process -FilePath "msiexec.exe" -ArgumentList "/i `"$installer`" /qn" -Verb RunAs -Wait
+
+    Write-Output "Script executed successfully!"
+} catch {
+    Write-Output "Error: $_"
+}
